8be4c7e8a43f08b01409d10584d8aa8925db6f92
lot-tola <2023463lot@aupp.edu.kh>
2026-03-01 21:29:24 +0700

    add linting check

--- main.go
+++ main.go
@@ -24,6 +24,12 @@ type apiConfig struct {
 //go:embed static/*
 var staticFiles embed.FS
 
+func unused() {
+	// something
+	// somehow
+
+}
+
 func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
